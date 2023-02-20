60b85bfbecdf07fe27c547dc3a15e0a198a309a3
Leon <onepieceleonnn@gmail.com>
2023-02-20 22:27:56 +0800

    add support for swiftPM

---
+++ Sources/FDFullscreenPopGesture/FDFullscreenPopGesture.swift
@@ -0,0 +1,6 @@
+public struct FDFullscreenPopGesture {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FDFullscreenPopGestureTests/FDFullscreenPopGestureTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FDFullscreenPopGesture
+
+final class FDFullscreenPopGestureTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FDFullscreenPopGesture().text, "Hello, World!")
+    }
+}
