aa65308e8496376a96074c298d51ef9b590fb6b6
Leon <onepieceleonnn@gmail.com>
2023-02-20 22:44:19 +0800

    remove useless code

--- Package.swift
+++ Package.swift
@@ -6,24 +6,14 @@ import PackageDescription
 let package = Package(
     name: "FDFullscreenPopGesture",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FDFullscreenPopGesture",
             targets: ["FDFullscreenPopGesture"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FDFullscreenPopGesture",
             dependencies: [],
             path: "FDFullscreenPopGesture"),
-//        .testTarget(
-//            name: "FDFullscreenPopGestureTests",
-//            dependencies: ["FDFullscreenPopGesture"]),
     ]
 )
